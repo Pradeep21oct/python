3043a800f49f12e6d158eabb591f46b0f22ad93e
pradeep <pradeep.ncst@gmail.com>
2025-01-18 18:47:13 -0500

    condition

---
+++ src/app/condition.py
@@ -0,0 +1,20 @@
+def conditin():
+    a = 10
+    print(type(a))
+    b = "hell0"
+    print(type(b))
+    #hello condtion
+    first_name =" Hello"
+    last_name = "ram"
+    print(first_name,last_name)
+    break_line = "hello " \
+                 "Ram"
+    print(break_line)
+    name = input("name please ")
+    print("Hello ", name)
+
+
+
+
+if __name__ == '__main__':
+    conditin()
