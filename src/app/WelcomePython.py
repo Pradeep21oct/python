ccbfc0e6260eea6da4460ad6ffc1d8f4732bdf15
pradeep <pradeep.ncst@gmail.com>
2025-01-18 18:10:38 -0500

    updated code

--- src/app/WelcomePython.py
+++ src/app/WelcomePython.py
@@ -1,5 +1,11 @@
 def print_hi(name):
     print(f'Hi, {name}')
+def add_num(a,b):
+    return a+b
 
 if __name__ == '__main__':
-    print_hi('Pradeep')
+    print_hi(f'Pradeep,{add_num(5,6)}')
+
+
+
+
