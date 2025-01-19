bc45d31459e4a0290f8a65092af0da7fd4b79554
pradeep <pradeep.ncst@gmail.com>
2025-01-18 19:34:42 -0500

    swap

---
+++ src/app/Swap.py
@@ -0,0 +1,14 @@
+def swap_num(a,b):
+    a,b=b,a
+    print(a,b)
+    x =19
+    if(x>10):
+        print("greater")
+    elif(x<10):
+        print("Less")
+    else:
+        print("equal")
+    for i in range(5):
+        print(i)
+if __name__ == '__main__':
+    swap_num(20,40)
